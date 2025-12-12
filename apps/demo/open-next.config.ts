707a9800f96ea9f74eca97c8ea1d49703cf2d58e
zavalit <zavalit@gmail.com>
2025-12-12 14:35:43 +0100

    add open-next config for cloudflare fix

--- apps/demo/open-next.config.ts
+++ apps/demo/open-next.config.ts
@@ -1,15 +1,2 @@
-import type { OpenNextConfig } from "@opennextjs/cloudflare";
-
-const config: OpenNextConfig = {
-    default: {
-        override: {
-            wrapper: "cloudflare-node",
-            converter: "edge",
-            incrementalCache: "dummy",
-            tagCache: "dummy",
-            queue: "dummy",
-        },
-    },
-};
-
-export default config;
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+export default defineCloudflareConfig();
